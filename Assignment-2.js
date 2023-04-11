04e8a7e350d400e48da0db0c7351d0b6bb61aed8
Daniyal Mushtaq <daniyal.musht@gmail.com>
2023-04-12 00:07:05 +0500

    add question 2

--- Assignment-2.js
+++ Assignment-2.js
@@ -1,15 +1,35 @@
 // Q.1
 
-function add_num(num1){
+// function add_num(num1){
 
-    return function (num2){
-        return num1 + num2;
-    };
-};
+//     return function (num2){
+//         return num1 + num2;
+//     };
+// };
 
-let sum = add_num(5);
-console.log(sum(6));
+// let sum = add_num(5);
+// console.log(sum(6));
 
+// Q.2
 
-// Q.2 
+// function FindNum(num, arr) {
+//     if(arr.length === 0)
+//     {
+//         return false;
+//     }
+    
+//     else if(arr[0] === num)
+//     {
+//         // flag = true;
+//         return true;
+//     }
+    
+//     return FindNum(num,arr.slice(1));
+
+// }
+
+// let arr = [1, 2, 3, 5, 6, 7];
+
+// console.log(FindNum(5,arr));
+
 
